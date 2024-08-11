8a0f493c2d65d9f9e76c6da66d57ba9ce440efaa
Nicolas Morales <0nicolasmorales@gmail.com>
2024-08-11 14:55:10 -0300

    fix

--- src/app/example/actions.js
+++ src/app/example/actions.js
@@ -1,18 +1,11 @@
 'use server'
 
-import { revalidatePath } from 'next/cache'
 import { deleteData, getData, updateData } from '../lib/api'
-import { redirect } from 'next/navigation'
 
 const EXAMPLE_PATH = "/example"
 
 export async function deleteExample(id){
     await deleteData(id)
-}
-
-export async function editExample(id, example){
-    await updateData(id, example)
-    redirect('/example/edit')
 }
 
 export async function getAllExamples(filter){
--- src/app/example/create/actions.js
+++ src/app/example/create/actions.js
@@ -1,13 +1,11 @@
 'use server'
 
 import { redirect } from 'next/navigation'
-import { revalidatePath } from 'next/cache'
 import {postData} from '@/app/lib/api'
 
 const EXAMPLE_PATH = "/example"
 
 export async function createExample(data){
     await postData(data)
-    revalidatePath(EXAMPLE_PATH)
     redirect(EXAMPLE_PATH)
 }
