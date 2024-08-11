4d22c8eab01a320e3ab622bafe5422ffd80fd004
Nicolas Morales <0nicolasmorales@gmail.com>
2024-08-11 18:39:12 -0300

    feat: navbar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,15 @@
-import { Inter } from "next/font/google";
+import { Inter } from "next/font/google"
+import { ChevronDownIcon, UserCircleIcon } from '@heroicons/react/24/solid'
+import localFont from 'next/font/local'
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const myFont = localFont({
+  src: './resources/font/GentyDemo-Regular.ttf',
+  display: 'swap',
+})
 
 export const metadata = {
   title: "PRUEBA",
@@ -11,7 +19,36 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+
+        <nav className="bg-white-800 text-white px-4 pt-4 pb-2 flex justify-between items-center border-b border-gray-300">
+
+          <div className="ml-10">
+            <a href="/" className={`${myFont.className} text-7xl shadow-gray-500 text-shadow-lg` }>
+              <span className="text-primary-blue">Adop</span>
+              <span className="text-primary-orange">tr</span>
+            </a>
+          </div>
+
+          <div className="flex items-center space-x-24 mr-6">
+            <div className="space-x-20">
+              <a href="/adopcion" className="text-primary-blue">Adoptar</a>
+              <a href="/example" className="text-primary-blue">Servicios</a>
+              <a href="/perdidas" className="text-primary-blue">Mascotas perdidas</a>
+              <a href="/novedades" className="text-primary-blue">Novedades</a>
+            </div>
+
+            <div className="relative">
+              <button className="flex items-center space-x-2 p-2 rounded">
+                <ChevronDownIcon className="w-4 h-4 text-custom-blue" />
+                <UserCircleIcon className="w-8 h-8 text-custom-blue" />
+              </button>
+            </div>
+          </div>
+        </nav>
+
+        {children}
+      </body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,12 +9,39 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'custom-blue': '#051A65',
+        'primary-blue': '#1C3EB2',
+        'primary-orange': '#FF823B',
+      },
+      spacing: {
+        '18': '4.5rem', 
+        '20': '5rem', 
+        '24': '6rem',
+        '28': '7rem',  
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '2px 4px 8px var(--tw-shadow-color)',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') },
+      )
+    }),
+  ],
 };
