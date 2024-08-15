e87cbf677d211a79611180a54985b8eba2cf6401
Vicku Miranda <vikymiri@hotmail.com>
2024-08-15 18:05:44 -0300

    Agrego funcionalidad Signup

--- src/app/signup/components/signupForm.jsx
+++ src/app/signup/components/signupForm.jsx
@@ -3,27 +3,28 @@
 import { useForm } from "react-hook-form";
 import { useRouter } from 'next/navigation';
 
-
-const LoginForm = () => {
+const SignupForm = () => {
     const router = useRouter();
-    
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, formState: { errors }, watch } = useForm();
+
+    // Obtén el valor de 'password' para la validación de 'confirmPassword'
+    const password = watch('password');
 
     const onSubmit = (data) => {
         console.log(data);
     };
 
     const handleRegisterClick = () => {
-        router.push('/signup'); 
+        router.push('/login'); 
     };
 
     return (
-        <section className="bg-white p-8 rounded-3xl shadow-lg w-full max-w-md mx-auto min-h-[40vh]">
+        <section className="bg-white p-8 rounded-3xl shadow-lg w-full max-w-md mx-auto min-h-[60vh]">
             <form
                 onSubmit={handleSubmit(onSubmit)}
                 className="flex flex-col space-y-4"
             >
-                <div className="mb-2">
+                <div>
                     <label htmlFor="email" className="block text-gray-700 text-sm font-medium">Correo Electrónico</label>
                     <input
                         type="email"
@@ -47,20 +48,35 @@ const LoginForm = () => {
                     {errors.password && <p className="text-red-500 text-sm mt-1">{errors.password.message}</p>}
                 </div>
 
+                <div>
+                    <label htmlFor="confirmPassword" className="block text-gray-700 text-sm font-medium">Confirmar Contraseña</label>
+                    <input
+                        type="password"
+                        id="confirmPassword"
+                        {...register('confirmPassword', {
+                            required: 'La confirmación de contraseña es obligatoria',
+                            validate: value => value === password || 'Las contraseñas no coinciden'
+                        })}
+                        className="w-full p-3 border border-gray-300 rounded-md"
+                        placeholder="Confirma tu contraseña"
+                    />
+                    {errors.confirmPassword && <p className="text-red-500 text-sm mt-1">{errors.confirmPassword.message}</p>}
+                </div>
+
                 <button
                     type="submit"
                     className="mx-auto py-2 px-4 bg-primary-blue text-white rounded-full hover:bg-primary-blue-dark transition-colors duration-300"
                 >
-                    Ingresar
+                    Registrarse
                 </button>
                 <p className="text-center text-sm mt-4">
-                    ¿No tenés una cuenta?{' '}
+                    ¿Ya tenés una cuenta?{' '}
                     <button 
                         type="button"
                         onClick={handleRegisterClick}
                         className="text-primary-orange underline hover:text-primary-orange-dark transition-colors duration-300 font-semibold"
                     >
-                        Registrarse
+                        Iniciar Sesión
                     </button>
                 </p>
             </form>
@@ -68,4 +84,4 @@ const LoginForm = () => {
     );
 }
 
-export default LoginForm;
+export default SignupForm;
