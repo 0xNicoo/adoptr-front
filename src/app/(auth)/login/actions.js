b75d20671085e9c8359a464f62891c5392360577
Nicolas Morales <0nicolasmorales@gmail.com>
2024-08-17 15:35:11 -0300

    feat: login redirect

--- src/app/(auth)/login/actions.js
+++ src/app/(auth)/login/actions.js
@@ -1,7 +1,11 @@
 'use server'
 
+import { redirect } from 'next/navigation'
 import { setSessionToken } from "../../../lib/session"
+
+const ROOT_PATH = "/"
 
 export async function login(data){
    await setSessionToken(data)
+   redirect(ROOT_PATH)
 }
