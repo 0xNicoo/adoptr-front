70498e19202a1cdb1206dd81a91b8f5915875fa8
kralsito <franco_kral@hotmail.com>
2024-08-20 16:29:19 -0300

    feature: branch de lista de publicaciones

---
+++ src/app/(root)/adopcion/components/sectionadop.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SectionAdop = () => {
+  return (
+    <div className="text-center mb-8 mt-16 p-6 bg-blue-100 border border-blue-300 rounded-3xl shadow-lg">
+      <h1 className="text-3xl font-bold text-blue-700">
+        Bienvenido al apartado de adopción de mascotas
+      </h1>
+    </div>
+  );
+};
+
+export default SectionAdop;
--- src/app/(root)/adopcion/page.jsx
+++ src/app/(root)/adopcion/page.jsx
@@ -1,6 +1,56 @@
+"use client";
+
+import React, { useState } from "react";
+import PaginationComponent from "./components/Pagination";
+import PublicationList from "./components/PublicationList";
+import SectionAdop from "./components/SectionAdop";
+
+const publications = [
+  { id: 1, name: "Pepito", age: "1 Año", image: "https://via.placeholder.com/300" },
+  { id: 2, name: "Juanito", age: "2 Años", image: "https://via.placeholder.com/300" },
+  { id: 3, name: "Dolo", age: "3 Años", image: "https://via.placeholder.com/300" },
+  { id: 4, name: "Vicky", age: "4 Años", image: "https://via.placeholder.com/300" },
+  { id: 5, name: "Nickito", age: "5 Años", image: "https://via.placeholder.com/300" },
+  { id: 6, name: "Franquito", age: "6 Años", image: "https://via.placeholder.com/300" },
+  { id: 7, name: "Sebas", age: "7 Años", image: "https://via.placeholder.com/300" },
+  { id: 8, name: "Die", age: "8 Años", image: "https://via.placeholder.com/300" },
+];
+
+const itemsPerPage = 6;
+
 export default function AdoptionPage() {
-    return (
-      <h1>ACA VA LA LISTA DE PUBLICACIONES</h1>
-    );
-  }
-  
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentPublications = publications.slice(startIndex, endIndex);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  return (
+    <div
+      className="flex flex-col justify-between items-center min-h-screen bg-cover bg-center"
+      style={{
+        backgroundImage: "url('/images/back.png')", 
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <SectionAdop />
+      <PublicationList
+        publications={currentPublications}
+        handlePageChange={handlePageChange}
+        currentPage={currentPage}
+      />
+      <div className="mt-8 mb-12">
+        <PaginationComponent
+          totalPages={Math.ceil(publications.length / itemsPerPage)}
+          currentPage={currentPage}
+          onPageChange={handlePageChange}
+        />
+      </div>
+    </div>
+  );
+}
