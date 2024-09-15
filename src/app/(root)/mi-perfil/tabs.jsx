e2db3072e321cae96ddfabd282e61e07addd9dac
dolodmg <oranotherway@gmail.com>
2024-09-15 18:45:33 -0300

    feature: función para hacer y ver posts en el perfil

---
+++ src/app/(root)/mi-perfil/tabs.jsx
@@ -0,0 +1,28 @@
+"use client"
+import { Tabs, Tab, Card, CardBody } from '@nextui-org/react';
+import AdoptionList from './adoption-list';
+import Post from './post';
+import PostList from './post-list';
+
+const MiPerfilTabs = ({ adoptions, posts, profile }) => {
+  return (
+    <Tabs aria-label="Options">
+      <Tab key="novedades" title="Novedades">
+      <Post />
+      <PostList posts={posts} profile={profile} />
+      </Tab>
+      <Tab key="publicaciones" title="Publicaciones de mascotas">
+        <AdoptionList adoptions={adoptions} />
+      </Tab>
+      <Tab key="servicios" title="Servicios">
+        <Card>
+          <CardBody>
+            <p>No se encontraron servicios</p>
+          </CardBody>
+        </Card>
+      </Tab>
+    </Tabs>
+  );
+};
+
+export default MiPerfilTabs;
---
+++ src/lib/api/post.js
@@ -0,0 +1,40 @@
+import 'server-only';
+import { getToken } from '../session';
+
+
+export async function getPosts() {
+    const token = await getToken();
+    const res = await fetch(`http://localhost:8080/post/all`, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      },
+      method: 'GET'
+    });
+  
+    if (!res.ok) {
+      const errorData = await res.json();
+      console.log(errorData)
+      throw new Error(`Failed to fetch data: `, errorData)
+    }
+    
+    return res.json();
+}
+
+export async function createPost(FormData) {
+    const token = await getToken();
+    const res = await fetch(`http://localhost:8080/post`, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      },
+      method: 'POST',
+      body: FormData
+    });
+  
+    if (!res.ok) {
+      const errorData = await res.json();
+      console.log(errorData)
+      throw new Error(`Failed to fetch data: `, errorData)
+    }
+    
+    return res.json();
+}
