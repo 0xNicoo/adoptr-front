e8957a4f0deafab680a65df4a4693f6ab34596b9
dolodmg <oranotherway@gmail.com>
2024-09-15 23:46:56 -0300

    feat: modal para eliminar un post

--- src/app/(root)/mi-perfil/actions.js
+++ src/app/(root)/mi-perfil/actions.js
@@ -4,6 +4,7 @@ import { getProfile } from "@/lib/api/profile";
 import { getAdoptions } from "@/lib/api/adoption";
 import { deleteAdoption } from "@/lib/api/adoption";
 import { getPosts } from "@/lib/api/post";
+import { deletePost } from "@/lib/api/post";
 import { createPost } from "@/lib/api/post";
 import { getToken } from "@/lib/session";
 import { jwtDecode } from "jwt-decode";
@@ -42,4 +43,8 @@ export async function handleGetPosts() {
 
 export async function handleCreatePost(FormData) {
   await createPost(FormData);
+}
+
+export async function deletePostAction(id){
+  await deletePost(id)
 }
--- src/app/(root)/mi-perfil/post-list.jsx
+++ src/app/(root)/mi-perfil/post-list.jsx
@@ -1,21 +1,21 @@
 "use client"
-import { Card, CardHeader, CardBody, CardFooter, Divider, Image } from '@nextui-org/react';
+import { Card, CardHeader, CardBody, CardFooter, Divider, Image, Button } from '@nextui-org/react';
 import { Inter } from "next/font/google";
 import { cilHeart } from '@coreui/icons';
 import { CIcon } from '@coreui/icons-react';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 const formatDate = (dateString) => {
   const postDate = new Date(dateString);
   const now = new Date();
-  const diffInMs = now - postDate; 
-  const diffInHours = Math.floor(diffInMs / (1000 * 60 * 60)); 
-  const diffInMins = Math.floor(diffInMs / (1000 * 60)); 
+  const diffInMs = now - postDate;
+  const diffInHours = Math.floor(diffInMs / (1000 * 60 * 60));
+  const diffInMins = Math.floor(diffInMs / (1000 * 60));
   if (diffInHours < 1) {
     return `Hace ${diffInMins} minuto${diffInMins !== 1 ? 's' : ''}`;
   } else if (diffInHours < 24) {
     return `Hace ${diffInHours} hora${diffInHours !== 1 ? 's' : ''}`;
   } else {
-    // Formato dd/mm/yyyy si pasaron más de 24 horas
     return new Intl.DateTimeFormat('es-ES', {
       day: '2-digit',
       month: '2-digit',
@@ -26,7 +26,7 @@ const formatDate = (dateString) => {
 
 const inter = Inter({ subsets: ["latin"] });
 
-const PostList = ({ posts, profile }) => {
+const PostList = ({ posts, profile, onOpen }) => {
   const sortedPosts = posts.sort((a, b) => new Date(b.date) - new Date(a.date));
 
   return (
@@ -34,24 +34,33 @@ const PostList = ({ posts, profile }) => {
       {sortedPosts.length > 0 ? (
         sortedPosts.map((post) => (
           <Card className="w-4/5" key={post.id}>
-            <CardHeader className="flex gap-3">
-              <Image
-                alt="Foto de perfil del usuario"
-                height={40}
-                width={40}
-                src={profile.s3Url}
-                className='rounded-full'
-              />
-              <div className="flex flex-col">
-                <p className="text-md">{profile.firstName} {profile.lastName}</p>
-                <p className="text-small text-default-500">{formatDate(post.date)}</p>
+            <CardHeader className="flex flex-row justify-between">
+              <div className='flex flex-row gap-2'>
+                <div className='flex flex-col'>
+                  <Image
+                    alt="Foto de perfil del usuario"
+                    height={40}
+                    width={40}
+                    src={profile.s3Url}
+                    className='rounded-full'
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <p className="text-md">{profile.firstName} {profile.lastName}</p>
+                  <p className="text-small text-default-500">{formatDate(post.date)}</p>
+                </div>
+              </div>
+              <div className='flex flex-col'>
+              <button onClick={() => onOpen(post.id)}>
+                <MoreVertIcon className='text-primary-blue'/>
+              </button>
               </div>
             </CardHeader>
-            <Divider/>
+            <Divider />
             <CardBody>
               <p>{post.description}</p>
             </CardBody>
-            <Divider/>
+            <Divider />
             <CardFooter>
               <CIcon icon={cilHeart} className="w-5 h-5 text-gray-500 mr-1" />
             </CardFooter>
