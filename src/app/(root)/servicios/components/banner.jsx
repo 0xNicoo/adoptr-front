32cc520aef6710f2df1214630b6f164beb416999
kralsito <franco_kral@hotmail.com>
2024-10-02 18:00:09 -0300

    feature: tipos de servicios responsive, banner

--- src/app/(root)/servicios/components/banner.jsx
+++ src/app/(root)/servicios/components/banner.jsx
@@ -2,27 +2,48 @@ import React from 'react';
 import { Roboto } from 'next/font/google';
 
 const roboto = Roboto({
-    subsets: ['latin'],
-    weight: ['400', '500', '700'],
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
 });
 
 const Banner = ({ description, imageUrl }) => {
   return (
-    <div className="relative bg-white-800 w-full h-80 md:h-[25rem] flex items-center justify-between shadow-md overflow-hidden">
-      
-      <div className="relative z-10 text-left px-7">
-        <h1 className={`text-8xl md:text-9xl font-bold mb-2 ${roboto.className}`}>
-          <span className="text-primary-blue">Servi</span>
-          <span className="text-primary-orange">cios</span>
-        </h1>
+    <>
+      {/* Banner para pantallas pequeñas */}
+      <div className="md:hidden relative bg-white-800 w-full h-60 flex flex-col items-center justify-center shadow-md overflow-hidden">
+        <div className="relative z-10 text-center px-4 mt-4">
+          <h1 className={`text-6xl lg:text-8xl font-bold mb-2 ${roboto.className}`}>
+            <span className="text-primary-blue">Servicios</span>
+          </h1>
+        </div>
+        <div className="absolute bottom-0 left-0 w-full flex justify-center">
+          <img
+            src='/images/mascotas_servicios.png' 
+            className="h-24 object-contain" 
+          />
+        </div>
+        <div className="absolute inset-0 bg-white opacity-5"></div>
       </div>
 
-      <div className="relative z-10 w-1/3">
-        <img src='/images/Servicio-2.png' alt="Descripción de la imagen" className="h-full object-cover" />
+
+      {/* Banner para pantallas medianas y grandes */}
+      <div
+        className="hidden md:flex relative w-full h-80 lg:h-[25rem] items-center justify-between shadow-md overflow-hidden"
+        style={{
+          backgroundImage: `url('/images/Background_services.jpg')`, 
+          backgroundSize: 'cover', 
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat', 
+        }}
+      >
+        <div className="relative z-10 text-left px-7">
+          <h1 className={`text-6xl lg:text-8xl font-bold mb-2 ${roboto.className}`}>
+            <span className="text-primary-blue">Servicios</span>
+          </h1>
+        </div>
       </div>
 
-      <div className="absolute inset-0 bg-black opacity-5"></div>
-    </div>
+    </>
   );
 };
 
